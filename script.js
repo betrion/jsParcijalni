6d2904eadbf2056c835a29a17c8bfb6f59fd6011
Dean Cvjetanovic <forteee@gmail.com>
2023-06-05 20:02:11 +0200

    styling choices

--- script.js
+++ script.js
@@ -1,32 +1,58 @@
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 const spinner = document.querySelector(".loading");
+const noResults = document.querySelector(".no-results");
+
 input.addEventListener("input", async (e) => {
+  if (e.target.value.length === 0) {
+    ul.innerHTML = "";
+    spinner.classList.add("hidden");
+    noResults.classList.add("hidden");
+    return;
+  }
   await getResults(e.target.value);
 });
 
 const getResults = async (searchTerm) => {
-  //makni spaceove iz inputa
   ul.innerHTML = "";
+  //makni spaceove iz inputa
   const filtriraniRezultat = searchTerm.replace(/ /g, "+");
+
   spinner.classList.remove("hidden");
-  const response = await fetch(
-    `https://itunes.apple.com/search?term=${filtriraniRezultat}&entity=song&limit=20`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
+  try {
+    const response = await fetch(
+      `https://itunes.apple.com/search?term=${filtriraniRezultat}&entity=song&limit=20`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      noResults.classList.remove("hidden");
+      throw new Error("Network response was not ok");
     }
-  );
 
-  const data = await response.json();
-  const rezultati = data.results;
+    noResults.classList.add("hidden");
+    const data = await response.json();
+    const rezultati = data.results;
+    console.log(rezultati);
+    if (rezultati.length === 0) {
+      noResults.classList.remove("hidden");
+    } else {
+      noResults.classList.add("hidden");
+      rezultati.forEach((pjesma) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<span><img src="${pjesma.artworkUrl100}"></span>${pjesma.trackName} - ${pjesma.artistName}`;
+        ul.appendChild(li);
+      });
+    }
+  } catch (error) {
+    noResults.classList.remove("hidden");
+    console.error("Error fetching data:", error);
+  }
 
-  rezultati.forEach((pjesma) => {
-    const li = document.createElement("li");
-    li.innerHTML = `<span class="song-pic"><img src="${pjesma.artworkUrl100}"></span>${pjesma.trackName} - ${pjesma.artistName}`;
-    ul.appendChild(li);
-  });
   spinner.classList.add("hidden");
 };
